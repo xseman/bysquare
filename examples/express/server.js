d76374f346b47a6fd9e8d39a8e245c54ce489af9
Filip Seman <seman.filip@gmail.com>
2021-06-19 12:26:21 +0200

    Add examples

---
+++ examples/express/server.js
@@ -0,0 +1,24 @@
+const { generate } = require("bysquare");
+const express = require("express");
+const app = express();
+
+const model = {
+    IBAN: "SK9611000000002918599669",
+    Amount: 100.0,
+    CurrencyCode: "EUR",
+    VariableSymbol: "123",
+    Payments: 1,
+    PaymentOptions: 1,
+    BankAccounts: 1,
+};
+
+app.use("/", express.static("./public"));
+app.get("/qr", async (_req, res) => {
+    const qrString = await generate(model);
+    res.send(qrString);
+});
+
+const port = 3_000;
+app.listen(3_000, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});

3a79a43ed685ab3f7f6fe2ac468ae56f5de12482
Filip Seman <filip.seman@protonmail.com>
2022-06-28 11:17:15 +0200

    examples: update package, apply formatting

--- examples/express/server.js
+++ examples/express/server.js
@@ -1,26 +1,26 @@
-const { generate } = require("bysquare");
-const express = require("express");
+const { generate } = require("bysquare")
+const express = require("express")
 
-const app = express();
+const app = express()
 
 /** @type {import("bysquare").Model} */
 const model = {
-    IBAN: "SK9611000000002918599669",
-    Amount: 100.0,
-    CurrencyCode: "EUR",
-    VariableSymbol: "123",
-    Payments: 1,
-    PaymentOptions: 1,
-    BankAccounts: 1,
-};
+	IBAN: "SK9611000000002918599669",
+	Amount: 100.0,
+	CurrencyCode: "EUR",
+	VariableSymbol: "123",
+	Payments: 1,
+	PaymentOptions: 1,
+	BankAccounts: 1
+}
 
-app.use("/", express.static("./public"));
+app.use("/", express.static("./public"))
 app.get("/qr", async (_req, res) => {
-    const qrString = await generate(model);
-    res.send(qrString);
-});
+	const qrString = await generate(model)
+	res.send(qrString)
+})
 
-const port = 3_000;
+const port = 3_000
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+	console.log(`Example app listening at http://localhost:${port}`)
+})

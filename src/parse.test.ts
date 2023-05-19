6904c386db04434897a69dfbb6c0f788b1b94b0d
Filip Seman <filip.seman@protonmail.com>
2023-05-19 12:45:24 +0200

    test: add parsing multiple dataset

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -72,3 +72,39 @@ export function header() {
 	throws(() => detect("aaaa"))
 	throws(() => detect("XXXX"))
 }
+
+export function multipleData() {
+	const data = new Map<string, DataModel>([
+		[
+			"0004I0006UC5LT8E21H3IC1K9R40P82GJL22NTU0586BBEOEKDMQSVUUBAOP1C0FFE14UJA1F1LJMV0FONE35J05TRC77FTIMV87NKNANNOFJB684000",
+			{
+				invoiceId: "2015001",
+				payments: [{
+					amount: 25.30,
+					currencyCode: CurrencyCode.EUR,
+					type: PaymentOptions.PaymentOrder,
+					bankAccounts: [{ iban: "SK4523585719461382368397" }],
+					beneficiary: { name: "John Doe" }
+				}]
+			}
+		],
+		[
+			"00054000DG4GL2L1JL66N01P4GCBG05KQEPULNMP9EB7MEE935VG4P4B1BDBN7MV4GU13R7DMGU9O93QEI2KQJLPTFFU7GJNP6QL0UADVHOQ3B0OP0OO5P4L58M918PG00",
+			{
+				invoiceId: "2015001",
+				payments: [{
+					amount: 45.55,
+					currencyCode: CurrencyCode.EUR,
+					type: PaymentOptions.PaymentOrder,
+					bankAccounts: [{ iban: "SK2738545237537948273958" }],
+					beneficiary: { name: "Jane Doe" },
+					paymentNote: "bendzín"
+				}]
+			}
+		]
+	])
+
+	for (const [qr, parsed] of data) {
+		deepEqual(parse(qr), parsed)
+	}
+}

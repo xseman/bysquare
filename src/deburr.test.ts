27e0a667dd7cae97d160cd818c557a886f89b25b
lukasbicus <ing.lukas.bicus@gmail.com>
2024-07-15 16:09:02 +0200

    Use deburr

--- src/deburr.test.ts
+++ src/deburr.test.ts
@@ -3,12 +3,12 @@ import test from "node:test";
 import {deburr, deburredLettersMap} from "./deburr.js";
 
 test("deburr", () => {
-	const deburred = deburr('Pôvodná faktúra')
+	const deburred = deburr("Pôvodná faktúra")
 	assert.equal(deburred, "Povodna faktura")
 })
 
-test('deburr can remove diacritic', () => {
-	const lettersWithDiacritic = Object.keys(deburredLettersMap).join('')
-	const expectedDeburredText = Object.values(deburredLettersMap).join('')
+test("deburr can remove diacritic", () => {
+	const lettersWithDiacritic = Object.keys(deburredLettersMap).join("")
+	const expectedDeburredText = Object.values(deburredLettersMap).join("")
 	assert.equal(expectedDeburredText, deburr(lettersWithDiacritic))
 })

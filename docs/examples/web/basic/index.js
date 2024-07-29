e09528f29c42dec4f1b40d78a3ea4b2368f8b639
lukasbicus <ing.lukas.bicus@gmail.com>
2024-07-29 06:38:32 +0200

    Use bysquare encode

--- docs/examples/web/basic/index.js
+++ docs/examples/web/basic/index.js
@@ -1,5 +1,18 @@
-function getProducedString(iban, amount, variable) {
-	return `IBAN: ${iban}, Amount: ${amount}, Variable: ${variable}`;
+import { CurrencyCode, encode, PaymentOptions } from "https://esm.sh/bysquare@2.8.3/";
+
+function getEncodedText(iban, amount, variable) {
+	return encode({
+		invoiceId: new Date().toLocaleDateString("sk"),
+		payments: [
+			{
+				type: PaymentOptions.PaymentOrder,
+				amount: amount,
+				bankAccounts: [{ iban: iban }],
+				currencyCode: CurrencyCode.EUR,
+				variableSymbol: variable
+			}
+		]
+	});
 }
 
 
@@ -7,21 +20,21 @@ function initializeForm() {
 	const amountInput = document.querySelector('input[name="amount"]');
 	const ibanInput = document.querySelector('input[name="iban"]');
 	const variableInput = document.querySelector('input[name="variable"]');
-	const producedStringInput = document.querySelector('#producedString');
+	const encodedTextDiv = document.querySelector('#encodedText');
 
 	amountInput.value = '100';
 	ibanInput.value = 'SK9611000000002918599669';
 	variableInput.value = '123';
 
-	producedStringInput.innerText = getProducedString(ibanInput.value, amountInput.value, variableInput.value);
+	encodedTextDiv.innerText = getEncodedText(ibanInput.value, amountInput.value, variableInput.value);
 
-	function updateProducedString() {
-		producedStringInput.innerText = getProducedString(ibanInput.value, amountInput.value, variableInput.value);
+	function updateEncodedText() {
+		encodedTextDiv.innerText = getEncodedText(ibanInput.value, amountInput.value, variableInput.value);
 	}
 
-	amountInput.addEventListener('input', updateProducedString);
-	ibanInput.addEventListener('input', updateProducedString);
-	variableInput.addEventListener('input', updateProducedString);
+	amountInput.addEventListener('input', updateEncodedText);
+	ibanInput.addEventListener('input', updateEncodedText);
+	variableInput.addEventListener('input', updateEncodedText);
 }
 
 window.addEventListener('load', initializeForm);

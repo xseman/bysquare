b15ef08a53365809a78f37f2b9960c317b3b0cd2
lukasbicus <ing.lukas.bicus@gmail.com>
2024-07-29 06:31:57 +0200

    Use div

--- docs/examples/web/basic/index.js
+++ docs/examples/web/basic/index.js
@@ -7,16 +7,16 @@ function initializeForm() {
 	const amountInput = document.querySelector('input[name="amount"]');
 	const ibanInput = document.querySelector('input[name="iban"]');
 	const variableInput = document.querySelector('input[name="variable"]');
-	const producedStringInput = document.querySelector('input[name="producedString"]');
+	const producedStringInput = document.querySelector('#producedString');
 
 	amountInput.value = '100';
 	ibanInput.value = 'SK9611000000002918599669';
 	variableInput.value = '123';
 
-	producedStringInput.value = getProducedString(ibanInput.value, amountInput.value, variableInput.value);
+	producedStringInput.innerText = getProducedString(ibanInput.value, amountInput.value, variableInput.value);
 
 	function updateProducedString() {
-		producedStringInput.value = getProducedString(ibanInput.value, amountInput.value, variableInput.value);
+		producedStringInput.innerText = getProducedString(ibanInput.value, amountInput.value, variableInput.value);
 	}
 
 	amountInput.addEventListener('input', updateProducedString);

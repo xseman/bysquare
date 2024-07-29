a6004ca851926e4236f4fba1e3a5831b45592128
lukasbicus <ing.lukas.bicus@gmail.com>
2024-07-29 06:47:30 +0200

    Render on canvas.

--- docs/examples/web/basic/index.js
+++ docs/examples/web/basic/index.js
@@ -1,4 +1,5 @@
-import { CurrencyCode, encode, PaymentOptions } from "https://esm.sh/bysquare@2.8.3/";
+import {CurrencyCode, encode, PaymentOptions} from "https://esm.sh/bysquare@2.8.3/";
+import {qrcanvas} from "https://esm.sh/qrcanvas@3.1.2/";
 
 function getEncodedText(iban, amount, variable) {
 	return encode({
@@ -7,12 +8,25 @@ function getEncodedText(iban, amount, variable) {
 			{
 				type: PaymentOptions.PaymentOrder,
 				amount: amount,
-				bankAccounts: [{ iban: iban }],
+				bankAccounts: [{iban: iban}],
 				currencyCode: CurrencyCode.EUR,
 				variableSymbol: variable
 			}
 		]
 	});
+}
+
+function renderOnCanvas(canvasEl, encodedText) {
+	if (canvasEl) {
+		const ctx = canvasEl.getContext("2d");
+		if (ctx) {
+			ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+			qrcanvas({
+				data: encodedText,
+				canvas: canvasEl
+			});
+		}
+	}
 }
 
 
@@ -21,15 +35,20 @@ function initializeForm() {
 	const ibanInput = document.querySelector('input[name="iban"]');
 	const variableInput = document.querySelector('input[name="variable"]');
 	const encodedTextDiv = document.querySelector('#encodedText');
+	const canvasEl = document.querySelector('#canvas');
 
 	amountInput.value = '100';
 	ibanInput.value = 'SK9611000000002918599669';
 	variableInput.value = '123';
 
-	encodedTextDiv.innerText = getEncodedText(ibanInput.value, amountInput.value, variableInput.value);
+	const encodedText = getEncodedText(ibanInput.value, amountInput.value, variableInput.value);
+	encodedTextDiv.innerText = encodedText;
+	renderOnCanvas(canvasEl, encodedText);
 
 	function updateEncodedText() {
-		encodedTextDiv.innerText = getEncodedText(ibanInput.value, amountInput.value, variableInput.value);
+		const encodedText = getEncodedText(ibanInput.value, amountInput.value, variableInput.value);
+		encodedTextDiv.innerText = encodedText;
+		renderOnCanvas(canvasEl, encodedText);
 	}
 
 	amountInput.addEventListener('input', updateEncodedText);

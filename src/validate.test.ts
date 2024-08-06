9cf47bc0ffb18f71349329564089ba61d7d03ad7
lukasbicus <ing.lukas.bicus@gmail.com>
2024-08-06 11:57:59 +0200

    Add dummy test

---
+++ src/validate.test.ts
@@ -0,0 +1,9 @@
+import assert from "node:assert";
+import test, {describe} from "node:test";
+
+describe('Group validate', () => {
+	test('validate' ,  () => {
+		// todo
+		assert.equal(true, false)
+	})
+})

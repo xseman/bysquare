6b22a502c9092301640d36549ab9dc43a054a62f
lukasbicus <ing.lukas.bicus@gmail.com>
2024-08-06 12:40:00 +0200

    Add validation of variable symbol

--- src/validate.test.ts
+++ src/validate.test.ts
@@ -1,6 +1,10 @@
 import assert from "node:assert";
 import test, { describe } from "node:test";
-import { validateCurrencyCode } from "./validate.js";
+import { CurrencyCode } from "./types.js";
+import {
+	validateCurrencyCode,
+	validateVariableSymbol,
+} from "./validate.js";
 
 describe("validate currency code", () => {
 	test("should pass for valid currency code", () => {
@@ -11,5 +15,22 @@ describe("validate currency code", () => {
 	});
 	test("should throw an error for invalid currency code", () => {
 		assert.throws(() => validateCurrencyCode("not currency code"));
+	});
+});
+
+describe("validate variable symbol", () => {
+	test("should pass for valid variable symbol", () => {
+		assert.doesNotThrow(() => {
+			const expected = 1231231231;
+			const result = validateVariableSymbol(String(expected));
+			assert.equal(result, expected);
+			const result2 = validateVariableSymbol(1231231231);
+			assert.equal(result2, expected);
+		});
+	});
+	test("should throw an error for invalid variable symbol", () => {
+		assert.throws(() => validateVariableSymbol("not variable symbol"));
+		assert.throws(() => validateVariableSymbol("-1"));
+		assert.throws(() => validateVariableSymbol(10000000000));
 	});
 });
--- src/validate.ts
+++ src/validate.ts
@@ -6,3 +6,18 @@ const CurrencyCodeSchema = z.nativeEnum(CurrencyCode);
 export function validateCurrencyCode(currencyCode: unknown) {
 	return CurrencyCodeSchema.parse(currencyCode);
 }
+
+const IntegerSchema = z.union([
+	z.string()
+		.regex(/^\d+$/, "Variable symbol must be a string representing an integer")
+		.transform((str) => parseInt(str, 10)),
+	z.number().int("Variable symbol must be an integer"),
+]);
+
+const VariableSymbolSchema2 = IntegerSchema.refine((num) => num >= 0 && num <= 9999999999, {
+	message: "Variable symbol must be between 0 and 9999999999",
+});
+
+export function validateVariableSymbol(variableSymbol: unknown) {
+	return VariableSymbolSchema2.parse(variableSymbol);
+}

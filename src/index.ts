d16187b90e6ba16da82af112479e5c182d481959
Filip Seman <filip.seman@pm.me>
2024-12-03 07:07:44 +0100

    chore: add license header

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright Filip Seman
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export { decode, detect, parse } from "./decode.js";
 export { encode, generate } from "./encode.js";
 export { validateDataModel, ValidationErrorMessage } from "./validations.js";
